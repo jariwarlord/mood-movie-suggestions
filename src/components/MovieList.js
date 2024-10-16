fcdc6e9681838314675f83111e512c27aacb4dd9
jariwarlord <berkedalar4@gmail.com>
2024-10-16 16:21:39 +0300

    update
    daha fazla film önerisi eklendi

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,54 +1,74 @@
-// movieList.js
-
-
 const movieSuggestions = {
     mutlu: [
-      'The Pursuit of Happyness', 'La La Land', 'Amélie', 'Forrest Gump',
-      'The Intouchables', 'Little Miss Sunshine', 'Up', 'Sing Street',
-      'Chef', 'Julie & Julia', 'The Secret Life of Walter Mitty',
-      'The Grand Budapest Hotel', 'Paddington', 'About Time',
-      'Zootopia', 'Yes Man', 'The Holiday', 'Ratatouille',
-      '500 Days of Summer', 'Mamma Mia!', 'The Greatest Showman',
-      'Coco', 'Wonder'
+        'The Pursuit of Happyness', 'La La Land', 'Amélie', 'Forrest Gump',
+        'The Intouchables', 'Little Miss Sunshine', 'Up', 'Sing Street',
+        'Chef', 'Julie & Julia', 'The Secret Life of Walter Mitty',
+        'The Grand Budapest Hotel', 'Paddington', 'About Time',
+        'Zootopia', 'Yes Man', 'The Holiday', 'Ratatouille',
+        '500 Days of Summer', 'Mamma Mia!', 'The Greatest Showman',
+        'Coco', 'Wonder'
     ],
     hüzünlü: [
-      'Schindler\'s List', 'Grave of the Fireflies', 'Requiem for a Dream',
-      'The Green Mile', 'Atonement', 'Blue Valentine', 'Manchester by the Sea',
-      'The Boy in the Striped Pajamas', 'Million Dollar Baby',
-      'The Fault in Our Stars', 'My Sister\'s Keeper', '12 Years a Slave',
-      'Eternal Sunshine of the Spotless Mind', 'The Pianist', 
-      'Dancer in the Dark', 'Leaving Las Vegas', 'Brokeback Mountain', 
-      'Hachi: A Dog\'s Tale', 'One Day', 'The Lovely Bones', 
-      'If Beale Street Could Talk', 'Lion'
+        'Schindler\'s List', 'Grave of the Fireflies', 'Requiem for a Dream',
+        'The Green Mile', 'Atonement', 'Blue Valentine', 'Manchester by the Sea',
+        'The Boy in the Striped Pajamas', 'Million Dollar Baby',
+        'The Fault in Our Stars', 'My Sister\'s Keeper', '12 Years a Slave',
+        'Eternal Sunshine of the Spotless Mind', 'The Pianist', 
+        'Dancer in the Dark', 'Leaving Las Vegas', 'Brokeback Mountain', 
+        'Hachi: A Dog\'s Tale', 'One Day', 'The Lovely Bones', 
+        'If Beale Street Could Talk', 'Lion'
     ],
     aksiyon: [
-      'Mad Max: Fury Road', 'Die Hard', 'John Wick', 'Venom', 'Blade Runner',
-      'Gladiator', 'The Dark Knight', 'Inception', 'The Bourne Identity',
-      'Terminator 2: Judgment Day', 'Fight Club', 'Casino Royale',
-      'Kill Bill: Vol. 1', 'The Matrix', 'Black Panther', 
-      'Mission: Impossible - Fallout', 'Deadpool', 'Logan', 
-      'Kingsman: The Secret Service', 'The Avengers', 
-      'Spider-Man: Homecoming', 'Fury', 'Dunkirk', 'The Equalizer'
+        'Mad Max: Fury Road', 'Die Hard', 'John Wick', 'Venom', 'Blade Runner',
+        'Gladiator', 'The Dark Knight', 'Inception', 'The Bourne Identity',
+        'Terminator 2: Judgment Day', 'Fight Club', 'Casino Royale',
+        'Kill Bill: Vol. 1', 'The Matrix', 'Black Panther', 'Mission: Impossible - Fallout',
+        'Deadpool', 'Logan', 'Kingsman: The Secret Service', 'The Avengers',
+        'Spider-Man: Homecoming', 'Fury', 'Dunkirk', 'The Equalizer'
     ],
     rastgele: [
-      'Bullet Train', 'Jumper', 'The Greatest Showman', 'Bright', 
-      'The Thing', 'Troy', 'Tenet', 'Butterfly Effect', 
-      'Hocus Pocus', "Dude Where's my Car", 'Tommy Boy', 
-      'The Passion of the Christ', 'Naked', 'John Carter', 
-      'Empire of the Sun' 
+        'Bullet Train', 'Jumper', 'The Greatest Showman','Bright', 'The Thing', 
+        'Troy' , 'Tenet', 'Butter Fly Effect', 'Hocus Pocus', 
+        "Dude Where's my Car", 'Tommy Boy', 'The Passion of the Christ',
+        'Naked', 'John Carter', 'Empire of the Sun' 
     ],
     anime: [
-      'Attack on Titan', 'My Hero Academia', 'Naruto', 'One Piece', 
-      'Death Note', 'Fullmetal Alchemist: Brotherhood', 
-      'Demon Slayer: Kimetsu no Yaiba', 'Sword Art Online', 
-      'Hunter x Hunter', 'Dragon Ball Z', 'Neon Genesis Evangelion', 
-      'Spirited Away', 'Your Name', 'Cowboy Bebop', 
-      'One Punch Man', 'Jujutsu Kaisen', 'Tokyo Ghoul', 
-      'Bleach', 'Fairy Tail', 'Mob Psycho 100', 
-      'The Promised Neverland', 'Fate/Zero', 'Steins;Gate', 
-      'Made in Abyss'
+        'Attack on Titan', 'My Hero Academia', 'Naruto', 'One Piece', 
+        'Death Note', 'Fullmetal Alchemist: Brotherhood', 'Demon Slayer: Kimetsu no Yaiba', 
+        'Sword Art Online', 'Hunter x Hunter', 'Dragon Ball Z',
+        'Neon Genesis Evangelion', 'Spirited Away', 'Your Name', 
+        'Cowboy Bebop', 'One Punch Man', 'Jujutsu Kaisen',
+        'Tokyo Ghoul', 'Bleach', 'Fairy Tail', 'Mob Psycho 100',
+        'The Promised Neverland', 'Fate/Zero', 'Steins;Gate', 'Made in Abyss'
     ],
-  };
-  
-  export default movieSuggestions;
-  
+    dram: [
+        'The Shawshank Redemption', 'Fight Club', 'Forrest Gump', 
+        'The Godfather', '12 Angry Men', 'The Dark Knight', 
+        'Pulp Fiction', 'Schindler\'s List', 'The Green Mile', 
+        'Goodfellas', 'The Silence of the Lambs', 'The Departed',
+        'A Beautiful Mind', 'The Social Network', 'The Pianist',
+        'The Revenant', 'Spotlight', 'The Imitation Game', 
+        'Parasite', 'Moonlight', 'Nomadland'
+    ],
+    romantizm: [
+        'The Notebook', 'Pride & Prejudice', 'Titanic', 
+        'La La Land', 'A Walk to Remember', 'Me Before You',
+        'The Fault in Our Stars', 'Crazy, Stupid, Love', 
+        'Eternal Sunshine of the Spotless Mind', '500 Days of Summer',
+        'When Harry Met Sally', 'Notting Hill', 'Call Me by Your Name',
+        'Silver Linings Playbook', 'To All the Boys I\'ve Loved Before',
+        'Always Be My Maybe', 'Love Actually', 'Sleepless in Seattle',
+        'Before Sunrise', 'The Vow'
+    ],
+    bilimKurgu: [
+        'Inception', 'The Matrix', 'Interstellar', 
+        'Blade Runner 2049', 'The Fifth Element', 'Arrival',
+        'Ex Machina', 'Minority Report', 'Eternal Sunshine of the Spotless Mind',
+        '2001: A Space Odyssey', 'Star Wars: A New Hope', 'Jurassic Park',
+        'The Terminator', 'Ghost in the Shell', 'District 9',
+        'The Martian', 'Ready Player One', 'Looper',
+        'Her', 'Annihilation'
+    ],
+};
+
+export default movieSuggestions;

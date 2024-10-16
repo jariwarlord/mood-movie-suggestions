61df956428b05ca392036a6a7bad7003f24ac23c
jariwarlord <berkedalar4@gmail.com>
2024-10-16 13:17:48 +0300

    mutlu ve hüzünlü filmler eklendi.

--- src/components/MovieSuggestions.js
+++ src/components/MovieSuggestions.js
@@ -31,9 +31,20 @@ const MovieSuggestions = ({ mood }) => {
     };
 
     const movieSuggestions = {
-      mutlu: ['The Pursuit of Happyness', 'La La Land', 'Amélie'],
-      hüzünlü: ['Schindler\'s List', 'A Beautiful Mind', 'The Green Mile'],
-      aksiyon: ['Mad Max: Fury Road', 'Die Hard', 'John Wick'],
+        mutlu : ['The Pursuit of Happyness', 'La La Land', 'Amélie','Forrest Gump','The Intouchables', 'Little Miss Sunshine', 'Up',            'Sing Street',
+            'Chef','Julie & Julia','The Secret Life of Walter Mitty', 'The Grand Budapest Hotel','Paddington','About Time',
+            'Zootopia','Yes Man','The Holiday','Ratatouille','500 Days of Summer','Mamma Mia!','The Greatest Showman',            'Coco',
+            'Wonder'],          
+            hüzünlü : [
+                'Schindler\'s List', 'Grave of the Fireflies', 'Requiem for a Dream', 'The Green Mile', 
+                'Atonement', 'Blue Valentine', 'Manchester by the Sea', 'The Boy in the Striped Pajamas', 
+                'Million Dollar Baby', 'The Fault in Our Stars', 'My Sister\'s Keeper', '12 Years a Slave', 
+                'Eternal Sunshine of the Spotless Mind', 'The Pianist', 'Dancer in the Dark', 
+                'Leaving Las Vegas', 'Brokeback Mountain', 'The Pursuit of Happyness', 'Hachi: A Dog\'s Tale', 
+                'One Day', 'The Lovely Bones', 'If Beale Street Could Talk', 'Lion'
+              ],
+      aksiyon: ['Mad Max: Fury Road', 'Die Hard', 'John Wick','Venom','Blade Runner'],
+      rastgele:['Bullet Train', 'Jumper','The Greatest Showman']
     };
 
     const normalizedMood = mood.toLowerCase();
